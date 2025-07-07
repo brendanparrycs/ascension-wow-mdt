afaf82ac84fd8a6eb3c499f68a094e4d0ed1faea
brendanparrycs <brendanparrycs@gmail.com>
2025-07-06 18:05:09 -0700

    Map display fixed
    Cropped out transparent pieces of map tiles. Next I need to work on displaying mobs correctly on the map.

--- src/util/map.js
+++ src/util/map.js
@@ -1,8 +1,10 @@
-const mapHeight = 768;
-const mapWidth = 1024;
-const maxCoords = [-mapHeight, mapWidth];
-// const yOffset = 51;
+import { point } from "leaflet";
 
-// export const mapCenter = [maxCoords[0] / 2 + yOffset, maxCoords[1] / 2];
+export const tileSize = point(250, 223);
+
+const mapHeight = tileSize.y * 3;
+const mapWidth = tileSize.x * 4;
+const maxCoords = [-mapHeight, mapWidth];
+
 export const mapCenter = [maxCoords[0] / 2, maxCoords[1] / 2];
 export const mapBounds = [[0, 0], maxCoords];
